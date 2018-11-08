ea6de94b026a2dd0e9a41b1aaf69c9278c0431a8
Amartya Yadav <1800947@rgu.ac.uk>
2018-11-08 13:41:12 +0000

    added comment

--- Resizing Tool.py
+++ Resizing Tool.py
@@ -10,4 +10,4 @@ r = 28.0 / image.shape[1]
 dim = (28, int(image.shape[0] * r))
 
 resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
-cv2.imshow("resized", resized)
+cv2.imshow("resized", resized) #whatever
